6d02c746b6e7c8fe9b2e052a5bf0ffd620078aa2
jslatteryCCC <61437489+jslatteryCCC@users.noreply.github.com>
2022-01-28 15:02:27 -0500

    Exercise 2-1 Completed

---
+++ FutureValues/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FutureValues.Models;
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        ViewBag.FV = 0;
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Index(FutureValuesModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+        }
+        else
+        {
+            ViewBag.FV = 0;
+        }
+        return View(model);
+    }
+}
