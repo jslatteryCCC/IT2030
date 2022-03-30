12d066c127efc086f3d22febb3f5923f80039ed9
jslatteryCCC <61437489+jslatteryCCC@users.noreply.github.com>
2022-03-30 19:09:29 -0400

    Initial Commit Proj 1

---
+++ Project1/Project1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1.Controllers {
+    public class HomeController : Controller {
+
+        private TripContext context { get; set; }
+
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, TripContext ctx) {  
+            _logger = logger;
+            context = ctx;
+        }
+
+        public IActionResult Index() {
+            var trips = context.Trips.OrderBy(t => t.Destination).ToList();
+            return View(trips);
+        }
+
+    }
+}
---
+++ Project1/Project1/Controllers/TripController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Project1.Models;
+using System.Text.Json;
+namespace Project1.Controllers {
+    public class TripController : Controller {
+
+        private TripContext context { get; set; }
+
+        public TripController(TripContext ctx) {
+            context = ctx;
+        }
+        [HttpGet]
+        public IActionResult LinkToCreateNewTrip() {
+            return View("CreateNewTrip", new Trip());
+        }
+
+        [HttpPost]
+        public IActionResult CreateNewTrip(Trip trip) {
+            context.Trips.Add(trip);
+
+            if (trip.AccommodationName == null) {
+                return View("EnterThingsToDo", trip);
+            }
+            return View("EnterAccommodationDetails", trip);
+        }
+        [HttpPost]
+        public IActionResult EnterAccommodationDetails(Trip trip) {
+            return View("EnterThingsToDo", trip);
+        }
+        [HttpPost] 
+        public IActionResult AddToDb(Trip trip) {
+            context.SaveChanges();
+
+            return View("Home", "Index");
+        }
+    }
+}
+
