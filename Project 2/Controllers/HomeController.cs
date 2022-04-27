9ec790cd3cd8db0bdaa8a4ee5ab2c666534f1967
jslatteryCCC <61437489+jslatteryCCC@users.noreply.github.com>
2022-04-27 17:15:36 -0400

    Project 2 Commit

---
+++ Project 2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_2.Controllers {
+    public class HomeController : Controller {
+
+        [HttpGet]
+        public IActionResult Index() {
+            ViewBag.DiscountAmount = 0;
+            ViewBag.Total = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceModel price) {
+            if (ModelState.IsValid) {
+                ViewBag.DiscountAmount = price.DiscountAmountCalculator((double)price.Subtotal, (double)price.DiscountPercent);
+                ViewBag.Total = price.TotalCalculator((double)price.Subtotal, (double)price.DiscountAmount);
+            }
+            else {
+                ViewBag.DiscountAmount = 0;
+                ViewBag.Total = 0;
+            }
+            return View("Index", price);
+        }
+
+    }
+
+}
---
+++ Project 2/Models/PriceModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_2.Models {
+    public class PriceModel {
+        [Required(ErrorMessage = "Please enter a valid subtotal.")]
+        [Range(1, 1000000, ErrorMessage = "Please enter a number between 1 and 1 million.")]
+        public double? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount percentage.")]
+        [Range(1, 100, ErrorMessage = "Please enter a number between 1 and 100.")]
+        public double? DiscountPercent { get; set; }
+        public double DiscountAmount { get; set; } = 0;
+        public double Total { get; set; } = 0;
+        public double DiscountAmountCalculator(double sub, double percent) => DiscountAmount = (sub * percent) / 100;
+        public double TotalCalculator(double s, double da) => Total = s - da;
+
+    }
+}
