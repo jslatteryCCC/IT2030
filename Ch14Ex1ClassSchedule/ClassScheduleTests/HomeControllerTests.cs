1c43359712289543d628f2e264e99625b011ee77
jslatteryCCC <61437489+jslatteryCCC@users.noreply.github.com>
2022-05-01 16:07:56 -0400

    Assignment 14 Commit

---
+++ Ch14Ex1ClassSchedule/ClassScheduleTests/HomeControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using ClassSchedule.Models;
+using ClassSchedule.Controllers;
+using Microsoft.AspNetCore.Http;
+
+
+namespace ClassScheduleTests {
+    public class HomeControllerTests {
+        [Fact]
+        public void IndexActionMethod_ReturnsAViewResult() {
+
+            var unit = new Mock<IClassScheduleUnitOfWork>();
+            var http = new Mock<IHttpContextAccessor>();
+            var classes = new Mock<IRepository<Class>>();
+            var days = new Mock<IRepository<Day>>();
+            unit.Setup(u => u.Classes).Returns(classes.Object);
+            unit.Setup(u => u.Days).Returns(days.Object);
+
+            var controller = new HomeController(unit.Object, http.Object);
+
+            var result = controller.Index(0);
+
+            Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
